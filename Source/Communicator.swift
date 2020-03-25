1e1217ea2decd264fb08d4f88e21d2490db91535
Valentin Vasiliu <vali15tnt51v@gmail.com>
2020-03-25 18:13:50 +0100

    Download colorized scan ply from 3Shape, again after xcode update

--- Source/Communicator.swift
+++ Source/Communicator.swift
@@ -337,31 +337,16 @@ public class Communicator {
             taskGroup.leave()
           }
         }
-      }
-    }
-    taskGroup.notify(queue: DispatchQueue.main, work: DispatchWorkItem(block: {
-      completion(successfullyDownloadedAll)
-    }))
-  }
-  
-  public func downloadColorizedScanOnly(ofCase cCase: CommunicateCase, toDirectoryURL path:URL,
-                                        completeOne: @escaping ()->(), completion: @escaping (Bool)->()) {
-    let taskGroup = DispatchGroup()
-    var successfullyDownloadedAll = true
-    for (_, scan) in cCase.scans.enumerated() {
-      taskGroup.enter()
-      // Download ply file
-      if let scanExtension = scan.fileType, scanExtension == "dcm", let scanType = scan.jawType, scanType == "upper", let scanHash = scan.hash {
-        let plyURL = URL(string: baseMetadataURL + "/api/cases/" + cCase.id + "/attachments/" + scanHash + "/ply")!
-        download(resource: plyURL) { data in
-          let fileURL = path.appendingPathComponent(scanHash + ".ply")
-          do {
-            try data?.write(to: fileURL)
-            completeOne()
-            taskGroup.leave()
-          } catch {
-            successfullyDownloadedAll = false
-            taskGroup.leave()
+        
+        if let scanExtension = scan.fileType, scanExtension == "dcm", let scanType = scan.jawType, scanType == "upper", let scanHash = scan.hash {
+          let plyURL = URL(string: baseMetadataURL + "/api/cases/" + cCase.id + "/attachments/" + scanHash + "/ply")!
+          download(resource: plyURL) { data in
+            let fileURL = path.appendingPathComponent(scanHash + ".ply")
+            do {
+              try data?.write(to: fileURL)
+            } catch {
+              successfullyDownloadedAll = false
+            }
           }
         }
       }
